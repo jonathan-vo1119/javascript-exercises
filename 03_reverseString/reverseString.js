7cae23d0f3480eaa783caa089c7f4b36a7b95f5d
Jon Vo <jonathan.vo1119@gmail.com>
2022-05-14 13:01:57 +1000

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const splitArray = string.split("");
+    let returnArray = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        returnArray += splitArray[i];
+    }
+    return returnArray;
 };
 
 // Do not edit below this line
