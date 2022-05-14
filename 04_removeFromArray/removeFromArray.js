4c0a010b462c848427c954dc6e01b5407ef129b8
Jon Vo <jonathan.vo1119@gmail.com>
2022-05-14 13:30:49 +1000

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
+    const delNums = Array.from(arguments);
+    const myArray = delNums.shift();
+    let returnArray = [];
 
+    for (let i = 0; i < myArray.length; i++) {
+        if (!delNums.includes(myArray[i])) {
+            returnArray.push(myArray[i]);
+        }
+    }
+    return returnArray;
 };
 
 // Do not edit below this line
