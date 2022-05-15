2f27642f37b9811d926c4765592df73c15429f96
Jon Vo <jonathan.vo1119@gmail.com>
2022-05-15 17:51:53 +1000

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let x = 0;
+    let y = 1;
+    let z;
+    if (num < 0) {
+        return 'OOPS';
+    } else if (num == 0) {
+        return 0;
+    } else if (num == 1) {
+        return 1;
+    }
+    for (let i = num - 1; i > 0; i--) {
+        z = x + y;
+        x = y;
+        y = z;
+    }
+    return z;
 };
 
 // Do not edit below this line
