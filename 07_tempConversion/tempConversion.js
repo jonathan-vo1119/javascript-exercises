b418bc930d38c8e9048cf5e9c23d7933cdef4aa5
Jon Vo <jonathan.vo1119@gmail.com>
2022-05-15 11:18:13 +1000

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let conversion = (temp - 32) * 5/9;
+  return Math.round(conversion * 10) / 10;    // round result to 1dp if nec.
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let conversion = (temp * 9/5) + 32;
+  return Math.round(conversion * 10) / 10;    // round result to 1dp if nec.
 };
 
 // Do not edit below this line
