0efd5e95fac1656cb1ce7acd96dca7ca6021df14
Jon Vo <jonathan.vo1119@gmail.com>
2022-05-15 17:38:52 +1000

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const modText = text.replace(/[!,.\s]/g, "");
+    modText2 = modText.toLowerCase();
+    const myArray = modText2.split("");
+    let i = 0;
+    let len = myArray.length - 1;
+    while (len > 2 * i) {                                       // len - i > i
+        if (myArray[i] != myArray[len - i]) {
+            return false;
+        }
+        i++;
+    }
+    return true;
 };
 
 // Do not edit below this line
