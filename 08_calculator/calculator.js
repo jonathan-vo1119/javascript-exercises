43cc5e87a43682e00c32d6d1b5362e76202a4375
Jon Vo <jonathan.vo1119@gmail.com>
2022-05-15 11:28:53 +1000

    Completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+  let sum = 0;
+  for (let i = 0; i < myArray.length; i++) {
+    sum += myArray[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(myArray) {
+  let product = 1;
+  for (let i = 0; i < myArray.length; i++) {
+    product *= myArray[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  let result = 1;
+  for (let i = 0; i < exp; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
