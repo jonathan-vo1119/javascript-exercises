d39066da5f1b43afebbd81f7ee20030817d2ddb0
Jon Vo <jonathan.vo1119@gmail.com>
2022-05-15 10:20:34 +1000

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof(num1) != 'number' || typeof(num2) != 'number') {
+        return 'ERROR';
+    } else if (num1 > num2) {
+        let tmp = num2;
+        num2 = num1;
+        num1 = tmp;
+    }
+    
+    let counter = num1;
+    let sum = 0;
+    while (counter <= num2) {
+        sum += counter;
+        counter++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
